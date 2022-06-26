bb4349864c244c36f901d533737a0e58cd96ae36
Harry Fischer <harry.fischer@guardian.co.uk>
2022-06-26 21:17:57 +0100

    update

--- assets/main.js
+++ assets/main.js
@@ -17,3 +17,15 @@ contactNavElement.addEventListener('click', () => {
     console.log(contactNavOverlay)
     // if ()
 })
+
+
+
+// add class to image parent (<p>)
+const contentWrapper = document.querySelector('.page-content')
+console.log(contentWrapper)
+const contentImages = contentWrapper.querySelectorAll('p img')
+console.log(contentImages)
+
+contentImages.forEach((el) => {
+    el.parentElement.classList.add('image-wrapper')
+})
