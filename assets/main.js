29c9a2d6c7dd13c3ecd367745f49a6d6de391191
Harry Fischer <harry.fischer@guardian.co.uk>
2022-07-31 17:30:55 +0100

    animated rectangles

--- assets/main.js
+++ assets/main.js
@@ -61,20 +61,58 @@ if (contentWrapper && fadeMeIn.length > 0) {
 }
 
 
+
+const rectangles = document.querySelectorAll('.rectangle');
+
+let scrollAmount = 0;
+let oldScrollAmount = 0;
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+  rectangles.forEach((rectangle, i) => {
+    if (scrollTop + window.innerHeight > rectangle.offsetTop) {
+      if (scrollTop > oldScrollAmount) {
+        scrollAmount--;  
+      } else if (scrollTop < oldScrollAmount) {
+        scrollAmount++;
+      }
+      if (rectangle.classList.contains("left")) {
+        rectangle.style.transform = `translateY(${scrollAmount * 0.15}px)`;
+      } else {
+        rectangle.style.transform = `translateY(${scrollAmount * -0.15}px)`;
+      }
+    }  
+  });
+  oldScrollAmount = scrollTop;
+});
+
+
+
+
+
+  var i, tabcontent, tablinks;
+tablinks = document.getElementsByClassName("tablinks");
+tablinks[0].setAttribute('id', 'defaultOpen')
+
 function openCity(evt, cityName) {
   // Declare all variables
-  var i, tabcontent, tablinks;
+
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  console.log(tablinks[0])
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+setTimeout(() => {
+  document.getElementById("defaultOpen").click();
+}, 1500)
+
+
